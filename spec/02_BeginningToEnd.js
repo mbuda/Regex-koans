c591d54bac21d213cb05265b5c03b0ef4da22604
Marta Buda <bmarta1991@o2.pl>
2014-08-04 11:37:34 +0200

    BeginningToEnd pass

--- spec/02_BeginningToEnd.js
+++ spec/02_BeginningToEnd.js
@@ -1,27 +1,28 @@
 describe("Beginning To End", function() {
 
   it('^ anchors RegEx to beginning of the string', function() {
-    expect( 'dog cat pony' ).not.toMatch(/^cat/);    // This one already works
-    
-    expect( 'dog cat pony' ).toMatch(/^___/);        // Fix this one so it passes
+    expect( 'dog cat pony' ).not.toMatch(/^cat/);
+
+    expect( 'dog cat pony' ).toMatch(/^dog/);
   });
-  
+
   it('$ anchors RegEx to the end of the string', function() {
-    expect( 'dog cat pony' ).not.toMatch(/cat$/);  // This one already works
-    
-    expect( 'dog cat pony' ).toMatch(/___$/);      // Fix this one so it passes
+    expect( 'dog cat pony' ).not.toMatch(/cat$/);
+
+    expect( 'dog cat pony' ).toMatch(/pony$/);
   });
-  
+
   it('use both to match the entire input', function() {
-    expect( 'dog cat pony' ).not.toMatch(/^cat$/);  // This one already works
-    
-    expect( 'dog cat pony' ).toMatch(/^___$/);      // Fix this one so it passes
+    expect( 'dog cat pony' ).not.toMatch(/^cat$/);
+
+    expect( 'dog cat pony' ).toMatch(/^dog cat pony$/);
   });
-  
+
   it('a RegEx without any anchors will match a substring anywhere in the input', function() {
-    // try using "bc" in the RegEx
-    
-    expect( 'abcd' ).toMatch(/___/);
+
+    expect( 'abcd' ).toMatch(/bc/);
+    expect( 'abcd' ).toMatch(/a/);
+    expect( 'abcd' ).toMatch(/cd/);
   });
 
 });
