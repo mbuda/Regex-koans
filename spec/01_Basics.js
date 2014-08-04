0411e667b1b2f0d942b76b0816fade00a57e7349
Marta Buda <bmarta1991@o2.pl>
2014-08-04 11:33:01 +0200

    Basics pass (that was way too easy :D)

--- spec/01_Basics.js
+++ spec/01_Basics.js
@@ -1,23 +1,23 @@
 describe("Basics", function() {
 
-	it('replace /^___$/ with /^a$/ to make RegEx match', function() {
-		expect('a').toMatch(/^___$/);
-	});
-	
-	it('a RegEx that is the same as the string will match', function() {
-		expect('abcd').toMatch(/^___$/);
-	});
-	
-	it('RegEx is case sensitive', function() {
-		expect('AbCd').toMatch(/^___$/);
-	});
-	
-	it('whitespace matters', function() {
-		expect('ab cd').toMatch(/^___$/);
-	});
-	
-	it('testing for does-not-match is often useful, too', function() {
-		expect('abcd').not.toMatch(/^abcd$/);
-	});
+  it('replace /^___$/ with /^a$/ to make RegEx match', function() {
+    expect('a').toMatch(/^a$/);
+  });
+
+  it('a RegEx that is the same as the string will match', function() {
+    expect('abcd').toMatch(/^abcd$/);
+  });
+
+  it('RegEx is case sensitive', function() {
+    expect('AbCd').toMatch(/^AbCd$/);
+  });
+
+  it('whitespace matters', function() {
+    expect('ab cd').toMatch(/^ab cd$/);
+  });
+
+  it('testing for does-not-match is often useful, too', function() {
+    expect('abcd').not.toMatch(/^abc$/);
+  });
 
 });
